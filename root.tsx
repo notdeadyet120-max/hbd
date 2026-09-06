1c7543af40340e525c4c338ca2dc82af4d6ef11e
notdeadyet120-max <notdeadyet120@gmail.com>
2026-09-07 01:49:54 +0530

    root.tsx

---
+++ root.tsx
@@ -0,0 +1,3 @@
+{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+{ rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
+{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700;900&family=Rajdhani:wght@400;500;600;700&display=swap" },
